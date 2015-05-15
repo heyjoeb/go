4a0830f5dd5ce89eef2daae3370b47c07889dfd2
heyjoeb <heyjoeb@hotmail.com>
2015-05-15 12:16:24 -0500

    ajustes de estilos y responsive, personalización páginas de errores

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,5 +2,8 @@ class ReportsController < ApplicationController
   def index
     @theftReports = TheftReport.all
     @storeReports = StoreReport.all
+    unless params[:q].blank?
+      @theftReports = @theftReports.where(bike_name: q)
+    end
   end
 end
